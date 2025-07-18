7fca2b2b91f4c3766acd765a10469ca5e730829a
Tomiwa <tomiwamole@gmail.com>
2025-07-18 21:52:18 +0100

    complete features  overhaul + event basics setup

--- app.ts
+++ app.ts
@@ -1,12 +1,11 @@
 // src/app.ts
 import express, { Application } from "express";
-import MainRoutes from "./routes/mainRoutes";
 import { errorHandler } from "./middlewares/errorHandler";
 
 import pg, { Pool, PoolClient } from "pg";
 
 import Database from "./config/db";
-import Main from "./controllers/main";
+import TaskRoutes from "./src/features/tasks/task.routes";
 
 class App {
   public app: Application;
@@ -29,7 +28,7 @@ class App {
   }
 
   private setRoutes(db: PoolClient): void {
-    this.app.use("/", new MainRoutes(this.db).getRouter());
+    this.app.use("/", new TaskRoutes(this.db).getRouter());
   }
 }
 
